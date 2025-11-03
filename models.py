174d32d21c69d0fc184faf93f4557709d8262ccc
visipuki <visipuki@mail.ru>
2025-11-03 23:46:37 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,15 +1,13 @@
-from dataclasses import dataclass
-from typing import List, Optional
+from dataclasses import dataclass, field
+from typing import List
 
 @dataclass
-class Reminder:
-    medicine: str
+class Tablet:
+    name: str
     dosage: str
-    time_of_day: str  # утро/день/вечер/ночь
-    time: str
-    comment: Optional[str] = None
 
 @dataclass
-class UserSchedule:
-    user_id: int
-    reminders: List[Reminder]
+class Notification:
+    time: str           # HH:MM формат
+    tablets: List[Tablet] = field(default_factory=list)
+    enabled: bool = True
